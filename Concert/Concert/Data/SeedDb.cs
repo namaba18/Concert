1ba5133abf932eb6160d9a5602c7daa2caab8bfe
NATALIA MANTILLA BARRIENTOS <nataliamantilla307633@correo.itm.edu.co>
2022-04-22 00:10:06 -0500

    Entities and View register

---
+++ Concert/Concert/Data/SeedDb.cs
@@ -0,0 +1,84 @@
+﻿using Concert.Data.Entities;
+
+namespace Concert.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckTicketAsync();
+        }
+
+        private async Task CheckTicketAsync()
+        {
+            if (! _context.Tickets.Any())
+            {
+                for (int i = 0; i < 1250; i++)
+                {
+                    _context.Tickets.Add(new Ticket
+                    {
+                        Id = i,
+                        WasUsed = false,
+                        Document = "",
+                        Name = "",
+                        Date = DateTime.Today,
+                        Entrace = new Entrace { Description = "Norte" }
+
+                    });
+                }
+                for (int i = 1250; i < 2500; i++)
+                {
+                    _context.Tickets.Add(new Ticket
+                    { 
+                        Id=i,
+                        WasUsed = false,
+                        Document = "",
+                        Name = "",
+                        Date = DateTime.Today,
+                        Entrace = new Entrace { Description = "Sur" }
+
+                    });
+                }
+                for (int i = 2500; i < 3750; i++)
+                {
+                    _context.Tickets.Add(new Ticket
+                    {
+                        Id = i,
+                        WasUsed = false,
+                        Document = "",
+                        Name = "",
+                        Date = DateTime.Today,
+                        Entrace = new Entrace { Description = "Oriente" }
+
+                    });
+                }
+                for (int i = 3750; i < 5000; i++)
+                {
+                    _context.Tickets.Add(new Ticket
+                    {
+                        Id = i,
+                        WasUsed = false,
+                        Document = "",
+                        Name = "",
+                        Date = DateTime.Today,
+                        Entrace = new Entrace { Description = "Occidente" }
+
+                    });
+                }
+                await _context.SaveChangesAsync();
+
+            }
+        }
+
+        
+    }
+}
+
---
+++ Concert/Concert/Models/TicketViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concert.Models
+{
+    public class TicketViewModel
+    {
+        [Display(Name ="Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Usado")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public bool WasUsed { get; set; }
+
+        [Display(Name = "Document")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public String Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public String Name { get; set; }
+
+        [Display(Name = "Fecha")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public DateTime Date { get; set; }
+
+        public String Entrace { get; set; }
+    }
+}
